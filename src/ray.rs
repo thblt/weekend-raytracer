0b8a6528635fb0a208d1e7871e262ddbf058d4bc
Thibault Polge <thibault@thb.lt>
2023-08-02 14:23:49 +0200

    Rename members of Ray

--- src/ray.rs
+++ src/ray.rs
@@ -1,16 +1,17 @@
 use crate::vec3::{Point3,Vec3};
 
 pub struct Ray {
-    orig: Point3,
-    dir: Vec3,
+    pub origin: Point3,
+    pub direction: Vec3,
 }
 
 impl Ray {
-    pub fn new(orig: Point3, dir: Vec3) -> Ray {
-        Ray { orig, dir }
+    pub fn new(origin: Point3, direction: Vec3) -> Ray {
+        Ray { origin, direction }
     }
 
     pub fn at(self, t: f64) -> Point3 {
-        self.orig + self.dir * t
+        self.origin + self.direction * t
     }
+
 }
