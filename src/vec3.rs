ff6debc00c170f483df2209d56245e70f24a47a5
Thibault Polge <thibault@thb.lt>
2023-08-09 09:51:53 +0200

    Make Vec3::new() const

--- src/vec3.rs
+++ src/vec3.rs
@@ -12,7 +12,7 @@ pub type Point3 = Vec3;
 pub type Color = Vec3;
 
 impl Vec3 {
-    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+    pub const fn new(x: f64, y: f64, z: f64) -> Vec3 {
         Vec3 { x, y, z }
     }
 
