7218dc092a363e4a557795d8cd39c7fb1d9df423
Thibault Polge <thibault@thb.lt>
2023-08-09 15:37:54 +0200

    Update main7 to use the new antialiased camera interface

--- src/main7.rs
+++ src/main7.rs
@@ -7,7 +7,7 @@ fn main() {
         Sphere::new(Point3::new(0.0,-100.5,-1.0), 100.0),
     );
 
-    let cam = Camera::new(1000, 16.0/9.0);
+    let cam = Camera::new(1000, 16.0/9.0, 1);
 
     let _ = cam.render(&world).write_ppm("image7.ppm");
 }
